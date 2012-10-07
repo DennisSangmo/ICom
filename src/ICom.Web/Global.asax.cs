9d9cf4e122df08dca20d67171b7672a9ecf5ae68
Dennis Sangmo <dennis.sangmo@gmail.com>
2012-10-07 12:05:15 +0200

    Nhibernate profiler

--- src/ICom.Web/Global.asax.cs
+++ src/ICom.Web/Global.asax.cs
@@ -2,7 +2,6 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using HibernatingRhinos.Profiler.Appender.NHibernate;
 using ICom.Web.App_Start;
 
 namespace ICom.Web
@@ -22,8 +21,7 @@ namespace ICom.Web
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             FluentNhibernateConfig.CreateConfig();
             StructureMapConfig.Register(FluentNhibernateConfig.GetSession());
-
-            NHibernateProfiler.Initialize();
+            NHibernateProfilerBootstrapper.PreStart();
         }
     }
 }
